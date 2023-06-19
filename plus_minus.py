9f0b3c383e66239b61664d45368ceb2a31432b2a
Anurup Sankriti <117533355+Anurup-Sankriti@users.noreply.github.com>
2023-06-19 14:13:06 +0530

    Create plus_minus.py

---
+++ plus_minus.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def plusMinus(arr):
+    pos = 0
+    neg = 0
+    z = 0
+    for i in arr:
+        if i > 0: pos += 1
+        if i<0: neg += 1
+        if i == 0: z += 1
+    print(pos/len(arr))
+    print(neg/len(arr))
+    print(z/len(arr))
+            
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
