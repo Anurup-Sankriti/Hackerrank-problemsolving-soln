50d6939b35750407e4d6550c3ae77663c0dcf9a2
Anurup Sankriti <117533355+Anurup-Sankriti@users.noreply.github.com>
2023-06-19 14:11:08 +0530

    Create Birthday_cake_candles.py

---
+++ Birthday_cake_candles.py
@@ -0,0 +1,37 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    descending = sorted(candles, reverse = True)
+    count = 1
+    for i in range(1,len(descending)):
+        if descending[i] == descending[0]:
+            count += 1
+        else:
+            break
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
